7d6a47c6324567729c6070c88e4a04b47c81a15a
Leong PH <leong@mail.com>
2020-11-01 18:52:59 +0800

    Initial Commit

---
+++ SecondProject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SecondProject
+//
+//  Created by Leong Ping How on 01/11/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
